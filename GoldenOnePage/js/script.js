9f15882ea452038a8b1f2868fa4ae728c5f1c7e7
nardo88 <52648122+nardo88@users.noreply.github.com>
2020-07-09 17:54:25 +0300

    Add files via upload

---
+++ GoldenOnePage/js/script.js
@@ -0,0 +1,64 @@
+﻿$(document).ready(function(){
+  $('.slider').slick({
+    slidesToShow: 3,
+    arrows: false,
+    responsive: [
+    {
+      breakpoint: 680,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true
+       }
+    }]
+  });
+});
+
+let navigation = $('.navigation');
+let scrollPos = $(window).scrollTop();
+let header = $('.header');
+let headerH = header.innerHeight();
+let burger = $('.burger');
+let nav = $('.nav_links');
+let nav_link = $('.nav_link');
+
+
+$(window).on('scroll load resize', function () {
+  let headerH = header.innerHeight();
+  let scrollPos = $(this).scrollTop();
+
+  if (scrollPos > headerH){
+    navigation.addClass('fixed');
+  } else {
+    navigation.removeClass('fixed');
+
+  }
+
+});
+
+$('.header_btn').click(function () {
+  $("html, body").animate({
+    scrollTop: $("#services").offset().top + 1
+  }, 700);
+});
+
+$('[data-scrol]').click(function (event) {
+  event.preventDefault();
+
+  let elementId = $(this).data('scrol');
+  let elementOffset = $(elementId).offset().top;
+ 
+   $('html, body').animate({
+      scrollTop: elementOffset + 1
+    }, 700);
+
+});
+
+burger.on('click', function () {
+  nav.toggleClass('open');
+  
+});
+
+nav_link.on('click', function () {
+  nav.removeClass('open');
+});
