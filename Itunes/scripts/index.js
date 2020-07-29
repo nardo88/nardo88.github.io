645c0253ef9846f6a1ac18195a790458ba3086b5
nardo88 <52648122+nardo88@users.noreply.github.com>
2020-07-29 11:56:35 +0300

    Add files via upload

---
+++ Itunes/scripts/index.js
@@ -0,0 +1,29 @@
+import {radioPlayerInit} from './radioPlayer.js'
+import {videoPlayerInit} from './videoPlayer.js'
+import {musicPlayerInit} from './musicPlayer.js'
+
+const playerBtn = document.querySelectorAll('.player-btn')
+const playerBlock = document.querySelectorAll('.player-block')
+const temp = document.querySelector('.temp')
+
+
+const diactivationPlayer = () =>{
+	temp.style.display = 'none'
+	playerBtn.forEach(item => item.classList.remove('active'))
+	playerBlock.forEach(item => item.classList.remove('active'))
+}
+
+
+playerBtn.forEach((btn, i) => btn.addEventListener('click', () =>{
+		diactivationPlayer()
+		btn.classList.add('active')
+		playerBlock[i].classList.add('active')
+	})
+)
+
+
+radioPlayerInit() 
+videoPlayerInit() 
+musicPlayerInit() 
+
+
